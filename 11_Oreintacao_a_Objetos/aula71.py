1db343e18ff03b5a80483e51260bd5ba5d72a0bf
Julio Cesar Dias Dos Santos <juliozulu2008@gmail.com>
2023-09-21 21:36:04 -0300

    Fim do Curso!!!

--- 11_Oreintacao_a_Objetos/aula71.py
+++ 11_Oreintacao_a_Objetos/aula71.py
@@ -3,49 +3,24 @@ Herança Múltipla
 """
 
 
-class Pessoa:
-    def __init__(self, nome, data=None, cpf=None, rg=None):
-        self.nome = nome
-        self.data_nascimento = data
-        self.cpf = cpf
-        self.rg = rg
-        print("Classe pessoa")
-
-    def imprimir_nome(self):
-        return self.nome
-
+class Funcionario:
     def trabalhar(self):
-        pass
+        print("Trabalhando")
 
 
-class Administrador(Pessoa):
+class FrontEnd(Funcionario):
     def trabalhar(self):
-        nome_classe = self.__class__.__name__
-        return f"Classe  {nome_classe} Supervisionando..."
+        print("FrontEnd")
 
 
-class Aluno(Pessoa):
-    def __init__(self, nome):
-        super().__init__(nome)
-        self.matricula = None
-        self.notas = None
-        print("Classe aluno")
-
-    def estudar(self):
-        return "Estudando..."
-
-class Professor(Pessoa):
-    def __init__(self, nome):
-        super().__init__(nome)
-        self.disciplinas = []
-
+class BackEnd(Funcionario):
     def trabalhar(self):
-        nome_classe = self.__class__.__name__
-        return f"Classe  {nome_classe} Ensinando..."
+        print("Backend")
 
 
-aluno1 = Aluno("JULIO")
-professor1 = Professor("Xavier")
-adm = Administrador("Joao")
-print(professor1.trabalhar())
-print(adm.trabalhar())
+class FullStack(FrontEnd, BackEnd): # por padrao ele busca o construtor mas caso nao encontre ele utiliza o 1 indice
+    pass
+
+
+julio = FullStack()
+julio.trabalhar()
